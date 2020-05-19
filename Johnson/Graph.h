5c57d2f8794af5a8c22108cf74b253dcb431a6af
Julia0212 <yulia.samartseva@gmail.com>
2020-05-19 18:01:40 +0300

    Johnson algorithm

--- Johnson/Graph.h
+++ Johnson/Graph.h
@@ -2,59 +2,189 @@
 #include <cstddef>
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-enum class graphType { undirected, directed };
-
-struct Node {
-	int vertex;
-	int weight;
-	Node* next;
-	Node(int x) {
-		this->vertex = x;
-	}
-};
+typedef std::pair<int, int> edge;
 
 
 class Graph
 {
 private:
-	void insertToList(int source, int destination, int weight) {
-		Node* p = adjList[source];
+	vector<vector<edge>> adjList;
+	int N;
+	int E;
 
-		while (p->vertex != destination && p->next != NULL)
-			p = p->next;
+	bool Bellman_Ford(std::vector<std::pair<int, edge>>& edges, std::vector<int>& cost, int src) {
+		cost[src] = 0;
+		for (int i = 1; i <= cost.size() - 1; ++i)
+		{
+			for (int j = 0; j < edges.size(); j++)
+			{
+				int u = edges[j].first;
+				int v = edges[j].second.first;
+				int weight = edges[j].second.second;
 
-		Node* new_node = new Node(destination);
-		new_node->weight = weight;
-		new_node->next = NULL;
-		p->next = new_node;
-		E++;
+				if (cost[u] != INT_MAX && cost[u] + weight < cost[v])
+					cost[v] = cost[u] + weight;
+			}
+		}
+
+		for (int i = 0; i < edges.size(); i++) {
+			int u = edges[i].first;
+			int v = edges[i].second.first;
+			int weight = edges[i].second.second;
+			if (cost[v] > cost[u] + weight) return false;
+		}
+
+		return true;
+	}
+
+	void Dijkstra(std::vector<std::vector<int>>& cost, int src) {
+		vector<int> dist;
+		dist.resize(cost.size() + 1);
+
+		vector<bool> visited;
+		visited.resize(cost.size() + 1);
+		priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
+		
+		for (int i = 0; i < cost.size(); i++) {
+			dist[i] = INT_MAX;
+			visited[i] = false;
+		}
+
+		dist[src] = 0;
+
+		pq.push(make_pair(0, src));
+
+		while (!pq.empty()) {
+			pair<int, int> temp = pq.top();
+			pq.pop();
+			int node = temp.second;
+			int d = temp.first;
+
+			if (!visited[node]) {
+				visited[node] = true;
+				for (int v = 0; v < cost.size(); v++) {
+					if (cost[node][v] != INT_MAX && dist[node] != INT_MAX
+						&& dist[node] + cost[node][v] < dist[v]) {
+						dist[v] = dist[node] + cost[node][v];
+						pq.push(make_pair(dist[v], v));
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < cost.size(); ++i) {
+			cost[src][i] = dist[i];
+		}
+
+	}
+
+	void outputCost(std::vector<std::vector<int>> cost) {
+		cout << "\nCost table: \n";
+		for (int i = 0; i < cost.size(); i++)
+		{
+			for (int j = 0; j < cost[i].size(); j++) {
+				if (cost[i][j] != INT_MAX)
+					cout << cost[i][j] << " ";
+				else cout << "inf ";
+			}
+			cout << "\n";
+		}
+	}
+
+	void initialiseCostTable(std::vector<std::vector<int>>& cost) {
+		for (int i = 0; i < adjList.size(); ++i) {
+			std::vector<int> help;
+			for (int k = 0; k < adjList.size(); ++k) {
+				help.push_back(INT_MAX);
+			}
+			help[i] = 0;
+			for (int j = 0; j < adjList[i].size(); ++j) {
+				help[adjList[i][j].first] = adjList[i][j].second;
+			}
+			cost.push_back(help);
+		}
+	}
+
+	std::vector<std::pair<int, edge>> getEdges() {
+		std::vector<std::pair<int, edge>> edges;
+		for (int i = 0; i < adjList.size(); ++i) {
+			for (int j = 0; j < adjList[i].size(); ++j) {
+				edges.push_back(std::make_pair(i, adjList[i][j]));
+			}
+		}
+		return edges;
 	}
 
 public:
-	vector<Node*> adjList;
-    int N;
-	int E;
-	graphType type;
 
-	Graph(int N, graphType type)
+	Graph(vector<vector<int>> input, int N)
 	{
+		this->N = N;
+		this->E = input.size();
+
 		adjList.resize(N);
-		this->type = type;
-        this->N = N;
-		this->E = 0;
+
+		for (int i = 0; i < E; i++) {
+			int source = input[i][0];
+			int destination = input[i][1];
+			int weight = input[i][2];
+			edge e = std::make_pair(destination - 1, weight);
+			adjList[source - 1].push_back(e);
+		}
 	}
 
-	void insert(int source, int destination, int weight) {
-		if (source > N || source < 0 || destination > N || destination < 0)
-			return;
-		if (type == graphType::undirected) insertToList(destination, source, weight);
+	void Johnson() {
+		std::vector<std::vector<int>> cost;
+		/*
+		cost[i][j] = 0 if i = j
+				   = e[i][j] if i->j
+				   = +infinity if i != j || i!->j
+		*/
+		initialiseCostTable(cost);
 
-		insertToList(source, destination, weight);
+		std::vector<std::pair<int, edge>> edges = getEdges();
+
+		//add new vertex to the graph (that is connected to all other, but with the cost 0)
+		for (int i = 0; i < adjList.size(); ++i) {
+			edges.push_back(std::make_pair(adjList.size(), std::make_pair(i, 0)));
+		}
+
+		std::vector<int> h;
+		for (int j = 0; j <= adjList.size(); ++j) {
+			h.push_back(INT_MAX);
+		}
+
+		if (!Bellman_Ford(edges, h, adjList.size())) std::cout << "\nThere is a negative cycle\n";
+		else {
+			//reweighting the graph using the bellman-ford calculations
+			for (int i = 0; i < cost.size(); ++i) {
+				for (int j = 0; j < cost[i].size(); ++j) {
+					if (cost[i][j] != INT_MAX)
+						cost[i][j] = cost[i][j] + h[i] - h[j];
+				}
+			}
+
+			for (int i = 0; i < adjList.size(); ++i) {
+				Dijkstra(cost, i);
+			}
+
+			for (int i = 0; i < cost.size(); ++i) {
+				for (int j = 0; j < cost[i].size(); ++j) {
+					if (cost[i][j] != INT_MAX)
+						cost[i][j] = cost[i][j] + h[j] - h[i];
+				}
+			}
+			outputCost(cost);
+		}
+		
+
 	}
 
 };
+
 
 
 
--- Johnson/Johnson.cpp
+++ Johnson/Johnson.cpp
@@ -2,8 +2,54 @@
 //
 
 #include <iostream>
+#include "Graph.h"
+
+void test1() {
+    std::cout << "\nTEST1";
+    vector<vector<int>> input;
+    vector<int> x;
+    input.push_back({ 1, 2, 10 });
+    input.push_back({ 1, 3, 10 });
+    input.push_back({ 2, 3, -10 });
+    input.push_back({ 3, 4, 20 });
+
+    //
+    Graph y(input, 4);
+    y.Johnson();
+}
+
+void test2() {
+    std::cout << "\nTEST2";
+    vector<vector<int>> input;
+    vector<int> x;
+    input.push_back({ 1, 2, -1 });
+    input.push_back({ 2, 3, -2 });
+    input.push_back({ 3, 1, -1 });
+    input.push_back({ 1, 4, 20 });
+
+    //
+    Graph y(input, 4);
+    y.Johnson();
+}
+
+void test3() {
+    std::cout << "\nTEST3";
+    vector<vector<int>> input;
+    vector<int> x;
+    input.push_back({ 1, 2, -5 });
+    input.push_back({ 2, 3, 4 });
+    input.push_back({ 1, 3, 2 });
+    input.push_back({ 1, 4, 3 });
+    input.push_back({ 3, 4, 1 });
+
+    //
+    Graph y(input, 4);
+    y.Johnson();
+}
 
 int main()
 {
-    
+    test1();
+    test2();
+    test3();
 }
